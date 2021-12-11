66340614bac01e4179c2a59418a315697f921545
Benn-Co <karitustephen@gmail.com>
2021-12-12 05:59:11 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World From Bot!";
+        return "Hello World From Bot!"; 
     }
 
     public static void main(String[] args) {
